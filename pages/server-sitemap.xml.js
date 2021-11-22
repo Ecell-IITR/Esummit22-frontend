3629eaf7e11b38799a28c792c0e9487932c057c9
Darkshredder <srivastavayash58@gmail.com>
2021-11-22 13:18:07 +0530

    Adds sitemap for seo

---
+++ pages/server-sitemap.xml.js
@@ -0,0 +1,19 @@
+import { getServerSideSitemap } from "next-sitemap";
+import { GetServerSideProps } from "next";
+import FetchApi from "../utils/fetchAPI";
+import { ALL_EVENTS_API, HOST_URL } from "../utils/APIs";
+export const getServerSideProps = async (ctx) => {
+  const { data } = await FetchApi("GET", ALL_EVENTS_API, null);
+
+  const fields = data.CompetitiveEvents.map(({ end_point }) => ({
+    loc: `${HOST_URL}/event/${end_point}`,
+    lastmod: new Date().toISOString(),
+  }));
+
+  return getServerSideSitemap(ctx, fields);
+};
+
+// Default export to prevent next.js errors
+const XML = () => {};
+
+export default XML;
