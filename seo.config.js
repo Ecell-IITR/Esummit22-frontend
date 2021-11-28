1e930c9826a89b380f4cdbec0da85b0848ad3cee
pranav <pranav_a@ece.iitr.ac.in>
2021-11-28 11:07:34 +0530

    changes in seo
    changed default title
    added Hashtags

--- seo.config.js
+++ seo.config.js
@@ -3,9 +3,9 @@ import { DefaultSeoProps } from "next-seo";
 export const SITE_DESCRIPTION = `E-Cell IIT Roorkee is Asia's one of the Largest Entrepreneurship Promoting college body. Our flagship event E-Summit, has hosted some of the most prominent stalwarts of the industry over the years including but not limited to Startup expo, Ideastorm, Investors Vista, Saleable, product-a-thon, speaker events and much more `;
 
 const defaultSEOConfig = {
-  defaultTitle: "E-summit 22 IIT Roorkee",
+  defaultTitle: "E-Summit 22 | IIT Roorkee ",
   description: SITE_DESCRIPTION,
-  
+
   openGraph: {
     type: "website",
     locale: "en_IN",
@@ -22,14 +22,18 @@ const defaultSEOConfig = {
     handle: "@ECell_IITR",
     cardType: "summary_large_image",
   },
-  additionalMetaTags : [
-    
-      { name: 'keywords',
-      content: 'esummit e-summit ecell entrepreneurship global summit 2022 iitr startup iit roorkee entrepreneur fest conference conclave Startup expo Ideastorm Investors Vista Saleable product-a-thon speaker events '}
-  ]
-
-  
+  additionalMetaTags: [
+    {
+      name: "keywords",
+      content:
+        "esummit e-summit ecell entrepreneurship global summit 2022 iitr startup iit roorkee entrepreneur fest conference conclave Startup expo Ideastorm Investors Vista Saleable product-a-thon speaker events ",
+    },
+    {
+      name: "Hahstags",
+      content:
+        " #FeteOfFortitude #esummit22 #ecelliitr #entrepreneur #entrepreneurship #entrepreneurial #HailingTheHustlers #ThinkersToDoers #InspiringInnovators #startup #iit #iitroorkee #iit_roorkee #iitr #indianinstituteoftechnology #entrepreneurindia #entrepreneurminds #entrepreneurialspirit",
+    },
+  ],
 };
-
 
 export default defaultSEOConfig;
