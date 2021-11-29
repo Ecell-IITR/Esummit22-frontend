b499004c722312d842fc23a83c903af95d753a3c
Darkshredder <srivastavayash58@gmail.com>
2021-11-30 03:10:02 +0530

    Adds font and some refactoring

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Raleway&display=optional"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Blinker&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
