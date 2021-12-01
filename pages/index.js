856971483b7a8efaa22dd8733cae8a3a2042246d
pranav <pranav_a@ece.iitr.ac.in>
2021-12-01 13:47:07 +0530

    mobile responsive

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,54 @@
 import Head from "next/head";
 import { Header } from "../components/Header/header";
 import { HeaderMobile } from "../components/Header/mobileHeader";
+import Script from "next/script";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  return <Header />;
+  const useWindowDimensions = () => {
+    const hasWindow = typeof window !== "undefined";
+
+    function getWindowDimensions() {
+      const width = hasWindow ? window.innerWidth : null;
+      const height = hasWindow ? window.innerHeight : null;
+      return {
+        width,
+        height,
+      };
+    }
+
+    const [windowDimensions, setWindowDimensions] = useState(
+      getWindowDimensions()
+    );
+
+    useEffect(() => {
+      if (hasWindow) {
+        function handleResize() {
+          setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+      }
+    }, [hasWindow]);
+
+    return windowDimensions;
+  };
+
+  const { height, width } = useWindowDimensions();
+  const breakpoint = 768;
+
+  return (
+    <>
+      {width <= breakpoint ? (
+        <div>
+          <HeaderMobile />
+        </div>
+      ) : (
+        <div>
+          <Header />
+        </div>
+      )}
+    </>
+  );
 }
