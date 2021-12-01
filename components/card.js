497fb2625e105e5a210baa2fdd5ffa794de21df8
pranav <pranav_a@ece.iitr.ac.in>
2021-12-01 18:30:50 +0530

    intial card

---
+++ components/card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div className="card">
+      <div class="trapezoid "></div>
+      <div class="rectangle"></div>
+      <div class="i_trapezoid "></div>
+    </div>
+  );
+}
