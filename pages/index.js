75826df9a7100587b1aadefda86f2450d36568c9
Akash Tiwari <akash_t@bt.iitr.ac.in>
2021-12-01 17:44:36 +0530

    create counter

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
 export default function Home() {
+<<<<<<< Updated upstream
   return <div>Hello Esummit</div>;
+=======
+  return (
+    <div className="h-screen overflow-hidden bg-black text-white flex flex-col space-y-8 items-center justify-center">
+      <Counter />
+    </div>
+  );
+>>>>>>> Stashed changes
 }
