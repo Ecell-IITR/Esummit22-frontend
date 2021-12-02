61a3e354615901de6516cbb5a5b72a3b654e033c
pranav <pranav_a@ece.iitr.ac.in>
2021-12-02 23:20:19 +0530

    mobile head update

--- components/Header/mobileHeader.js
+++ components/Header/mobileHeader.js
@@ -2,12 +2,13 @@ export const HeaderMobile = () => {
   return (
     <div className="mobile-navbar-majorcontainer">
       <input type="checkbox" />
+
       <div className="Hamburger">
         <div className="bar1"></div>
         <div className="bar2"></div>
         <div className="bar3"></div>
       </div>
-      <div>
+      <div className="image">
         <img
           src="fete-of-fortitude.png"
           alt="navbar-log"
@@ -35,96 +36,7 @@ export const HeaderMobile = () => {
         <li className="mobilenavMenu-items">
           <div className="mobilenavMenu-links">FAQ</div>
         </li>
-        {/* <li className="mobilenavMenu-items">
-                          <NavHashLink to="/#footer" className={"mobilenavMenu-links"} rel="noopener noreferrer">
-                            Contact Us
-                          </NavHashLink>
-                        </li> */}
       </ul>
-      {/* <img src={vector} alt="NOthing" className="mobilenavMenu-vector" />
-            <ul className={"mobilenavMenu-subcontent2"}>
-              {!localStorage.getItem("user_token") ? (
-                <li className="mobilenavMenu-items">
-                  <Link
-                    to="/login"
-                    className="mobilenavMenu-links"
-                    rel="noopener noreferrer"
-                  >
-                    Login
-                  </Link>
-                </li>
-              ) : null}
-              {!localStorage.getItem("user_token") ? (
-                <li className="mobilenavMenu-items">
-                  <p className="or-mobilenavMenu"></p>
-                </li>
-              ) : (
-                <li
-                  className="mobilenavMenu-items"
-                  // style={{ marginLeft: "30px" }}
-                >
-                  {localStorage.getItem("user_role_type") == "CA" ? (
-                    <Link
-                      to="/campus-ambassador"
-                      className={"mobilenavMenu-links"}
-                      rel="noopener noreferrer"
-                    >
-                      <div
-                        className="mobilenavMenu-clip clip"
-                        style={{ color: "6f00ff" }}
-                      >
-                        DashBoard
-                      </div>
-                    </Link>
-                  ) : (
-                    <Link
-                      to="/dashboard"
-                      className={"mobilenavMenu-links"}
-                      rel="noopener noreferrer"
-                    >
-                      <div
-                        className="mobilenavMenu-clip clip"
-                        style={{ color: "6f00ff" }}
-                      >
-                        DashBoard
-                      </div>
-                    </Link>
-                  )}
-                </li>
-              )}
-              {!localStorage.getItem("user_token") ? (
-                <li className="mobilenavMenu-items">
-                  <Link
-                    to="/register"
-                    className={"mobilenavMenu-links"}
-                    rel="noopener noreferrer"
-                  >
-                    <div className="mobilenavMenu-clip clip">Register</div>
-                  </Link>
-                </li>
-              ) : (
-                <li className="mobilenavMenu-items">
-                  <div
-                    onClick={this.handleLogout}
-                    className={"mobilenavMenu-links logout-link"}
-                    rel="noopener noreferrer"
-                  >
-                    <span style={{ backgroundColor: "transparent" }}>Logout</span>
-                  </div>
-                </li>
-              )}
-            </ul>
-          </div>
-          <div className="mobilenavMenu-esummit">
-            <img
-              src={esummitLogo}
-              alt="Esummit logo"
-              className="mobilenavMenu-logo"
-            /> */}
-      {/* <span>E-Summit 2021 IIT Roorkee</span>
-            <span>E-Cell Office, SAC Building</span>
-            <span>IIT Roorkee Roorkee</span>
-            <span>Uttrakhand - 247667</span> */}
     </div>
   );
 };
