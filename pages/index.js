a177146e8291c5a9af19ff5ba70f2d3b0a0f4512
Darkshredder <srivastavayash58@gmail.com>
2021-12-03 15:40:30 +0530

    Minor fixes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
+import Counter from "../components/section/counter";
+
 export default function Home() {
-<<<<<<< Updated upstream
-  return <div>Hello Esummit</div>;
-=======
   return (
     <div className="h-screen overflow-hidden bg-black text-white flex flex-col space-y-8 items-center justify-center">
       <Counter />
     </div>
   );
->>>>>>> Stashed changes
 }
