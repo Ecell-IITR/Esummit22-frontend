8ddec81b4dfaf06b670b875319301e847544cc4d
Akash Tiwari <80945514+sky-39@users.noreply.github.com>
2021-12-03 15:41:11 +0530

    create counter (#13)
    * create counter
    
    * change gradient and reduce animation time
    
    * reduce border thickness
    
    * Minor fixes
    
    Co-authored-by: Darkshredder <srivastavayash58@gmail.com>

---
+++ components/section/counter.js
@@ -0,0 +1,32 @@
+//import vector from "../../../../assets/sponsor1.svg";
+import CountUp, { useCountUp } from "react-countup";
+import VisibilitySensor from "react-visibility-sensor";
+
+export const Counter = () => {
+  const countertabs = [
+    { number: 20, text: "EVENTS", suffix: "+" },
+    { number: 6000, text: "FOOTFALL", suffix: "+" }, //30+
+    { number: 50, text: "SPEAKERS", suffix: "+" }, //24hrs+
+    { number: 500, text: "PRIZES", suffix: "+" }, //6000+
+  ];
+  return (
+    <div className="landing-counter-container">
+      {countertabs.map((tab) => (
+        // eslint-disable-next-line react/jsx-key
+        <div className="landing-counter-subcontainer">
+          <CountUp end={tab.number} suffix={tab.suffix} duration={1.5}>
+            {({ countUpRef, start }) => (
+              <VisibilitySensor onChange={start} delayedCall>
+                <span ref={countUpRef} />
+              </VisibilitySensor>
+            )}
+          </CountUp>
+          <img className="landing-counter-svg" src="vector.png" alt='landing counter image' />
+          <span className="landing-counter-text">{tab.text}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Counter;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import Counter from "../components/section/counter";
+
 export default function Home() {
-  return <div>Hello Esummit</div>;
+  return (
+    <div className="h-screen overflow-hidden bg-black text-white flex flex-col space-y-8 items-center justify-center">
+      <Counter />
+    </div>
+  );
 }
