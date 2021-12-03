756df474551d71ca4bcc13744805926a13555e0f
Darkshredder <srivastavayash58@gmail.com>
2021-12-04 03:40:21 +0530

    Linted

--- components/perks.js
+++ components/perks.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from "react";
 
 export const Perk = () => {
-    return <div className ="perks-container-behind">
-        <div className = "perks-container-front">
-            <div className="title_div">
-                <h1>Discounts</h1>
-                <img src="border_perks.png" alt="no-img"></img>
-            </div>
-            {/* <div className="border_div"><img src="border_perks.png" alt="no-img"></img></div> */}
-            <div className="content_div"><p className="content_style">Special discounts for participants on the main Summit registration.</p></div>
+  return (
+    <div className="perks-container-behind">
+      <div className="perks-container-front">
+        <div className="title_div">
+          <h1>Discounts</h1>
+          <img src="border_perks.png" alt="no-img"></img>
         </div>
+        <div className="content_div">
+          <p className="content_style">
+            Special discounts for participants on the main Summit registration.
+          </p>
+        </div>
+      </div>
     </div>
+  );
 };
 
 export default Perk;
