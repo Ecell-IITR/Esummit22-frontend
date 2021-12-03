674b237e4bcedbcb4e9a4697040fa2683eee19d0
pranav <pranav_a@ece.iitr.ac.in>
2021-12-03 09:31:45 +0530

    payNowcard update

--- components/payNowCard.js
+++ components/payNowCard.js
@@ -2,15 +2,18 @@ import React from "react";
 
 export default function PayNowCard() {
   return (
-    <div className="professional-pay-container">
-      <div class="rectangle_grad"></div>
-      <div class="rectangle"></div>
-      <div className="pp-botton">
-        <div className="pp-text">
-          <div className="pp-text-1">PAY NOW</div>
-          <div className="pp-text-2">Presented by E-Cell IITR</div>
+    <>
+      <div className="professional-pay-container">
+        <div className="pp-botton">
+          <div className="pp-text">
+            <div className="pp-text-1">PAY NOW</div>
+            <div className="pp-text-2">Presented by E-Cell IITR</div>
+          </div>
         </div>
       </div>
-    </div>
+      <div class="rectangle_grad">
+        <div class="rectangle"></div>
+      </div>
+    </>
   );
 }
