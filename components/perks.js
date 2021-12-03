807728a3ead2c11c5aac12a5266abf133bc0fa0b
Lomsha Agrawal <75161534+Lomsha123@users.noreply.github.com>
2021-12-04 03:41:10 +0530

    perk_component create (#16)
    * perk_component create
    
    * Linted
    
    Co-authored-by: Darkshredder <srivastavayash58@gmail.com>

---
+++ components/perks.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Perk = () => {
+  return (
+    <div className="perks-container-behind">
+      <div className="perks-container-front">
+        <div className="title_div">
+          <h1>Discounts</h1>
+          <img src="border_perks.png" alt="no-img"></img>
+        </div>
+        <div className="content_div">
+          <p className="content_style">
+            Special discounts for participants on the main Summit registration.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Perk;
