b8c3a294f95d98645d73f59b39160aaa3ebe806b
Lomsha123 <lomsha@ce.iitr.ac.in>
2021-12-04 17:31:03 +0530

    create sponsors_component

---
+++ components/sponsors.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Sponsor = () => {
+    return <div className="sponsor_main">
+        <div className="img_div_behind">
+            <div className="img_div_front">
+                <img src="amazon_PNG7_2.png" alt="no_image"></img>
+            </div>
+        </div>
+        <div className="title_sponsor">
+            <h1>Title Sponsor</h1>
+        </div>
+    </div>
+};
+
+export default Sponsor;
