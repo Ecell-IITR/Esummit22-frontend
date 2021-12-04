c25f970f05f9d43d51cd86cfd26bffd338465967
pranav <76247369+pranav-iitr@users.noreply.github.com>
2021-12-04 05:40:54 +0530

    created  Pay Now Card (#14)
    * intial card
    
    * intial changes
    
    * gradaint update
    
    * aligment corrected
    
    * Update file names
    
    * style update
    
    * payNowcard update
    
    * Update payNowCard.scss
    
    * Fixed some css issues
    
    Co-authored-by: Darkshredder <srivastavayash58@gmail.com>

---
+++ components/payNowCard.js
@@ -0,0 +1,12 @@
+export default function PayNowCard() {
+  return (
+    <div className="pay-outer-container">
+      <div className="pp-button">
+        <div className="pp-text">
+          <div className="pp-text-1">PAY NOW</div>
+          <div className="pp-text-2">Presented by E-Cell IITR</div>
+        </div>
+      </div>
+    </div>
+  );
+}
