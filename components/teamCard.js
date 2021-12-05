43e536e4557d0f868ebc211181b24c631fcba3fd
Akash Tiwari <akash_t@bt.iitr.ac.in>
2021-12-05 15:18:35 +0530

    create-team-card

---
+++ components/teamCard.js
@@ -0,0 +1,48 @@
+export const TeamMemberCard = () => {
+  return (
+    <div className="member-container">
+      <div className="main">
+        <div className="member-card-post" id="main-post">
+          Co-Convenor
+        </div>
+        <img src="vaibhav.png" className="member-card-image" alt="member" />
+      </div>
+      <div className="team-card-box">
+        <div className="member-card-name" id="main-name">
+          VAIBHAV SETHIA
+        </div>
+        <div className="team-card-description-hidden">
+          <div className="mb-2">Session Type : </div>
+          <div className="mb-2">Date : </div>
+          <div className="mb-2">Time : </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TeamMemberCard;
+{
+  /* <div className="hovered">
+          <div className="social-links">
+            <span className="social-links">
+              <a href="@vaibhav" target="_blank">
+                <img
+                  src="@vaibhav"
+                  className="social-links-handle"
+                  alt="social"
+                />
+              </a>
+            </span>
+            <span className="social-links">
+              <a href="@vaibhav" target="_blank">
+                <img
+                  src="@linkedin"
+                  className="social-links-handle"
+                  alt="social"
+                />
+              </a>
+            </span>
+          </div>
+        </div> */
+}
