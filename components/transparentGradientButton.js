e0050c87236b818eff7d784c7ebf56f8b33df10c
Akash Tiwari <akash_t@bt.iitr.ac.in>
2021-12-05 17:57:59 +0530

    create-event-head

---
+++ components/transparentGradientButton.js
@@ -0,0 +1,5 @@
+export const TransparentGradientButton = ({ text }) => {
+  return <div className="gradient-button-container">{text}</div>;
+};
+
+export default TransparentGradientButton;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Counter from "../components/section/counter";
+import EventHead from "../components/Events/eventHeadcard";
 export default function Home() {
   return (
     <div className="h-screen overflow-hidden bg-black text-white flex flex-col space-y-8 items-center justify-center">
