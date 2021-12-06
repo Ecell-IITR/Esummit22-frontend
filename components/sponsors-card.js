a18940ea4e8fca9dae696483550f6b1ac075cb67
Lomsha Agrawal <75161534+Lomsha123@users.noreply.github.com>
2021-12-07 00:36:05 +0530

    create sponsor_page (#23)
    * create sponsor_page

--- components/sponsors-card.js
+++ components/sponsors-card.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-export const Sponsor = () => {
+export const Sponsor = ({ sponsorsTitle, format }) => {
   return (
     <div className="sponsor_main">
       <div className="img_div_behind">
         <div className="img_div_front">
-          <img src="amazon_PNG7_2.png" alt="no_image"></img>
+          <img src={"sponsors/" + sponsorsTitle + format} alt=" "></img>
         </div>
       </div>
       <div className="title_sponsor">
-        <h1>Title Sponsor</h1>
+        <h1>{sponsorsTitle}</h1>
       </div>
     </div>
   );
