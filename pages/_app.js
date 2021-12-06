cfaafbecfee00a180b1ac256fb8e9e2dad35226b
Darkshredder <srivastavayash58@gmail.com>
2021-12-07 01:06:11 +0530

    Removed faq from _app.js
    Signed-off-by: Darkshredder <srivastavayash58@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -46,7 +46,6 @@ function MyApp({ Component, pageProps }) {
         </Script>
         <Header />
         <Component {...pageProps} />
-        <Faq />
         <Footer />
         <PayNowCard />
       </MobileProvider>
