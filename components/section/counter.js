c99d20e1bea1b700af69aeeb314177f0f688684a
Anand Jee Choubey <anandjeechoubey@Anands-MacBook-Air.local>
2021-12-07 12:46:52 +0530

    [wip]Scroll Animation

--- components/section/counter.js
+++ components/section/counter.js
@@ -2,6 +2,8 @@
 import CountUp, { useCountUp } from "react-countup";
 import VisibilitySensor from "react-visibility-sensor";
 
+import Divider from "../Divider";
+
 export const Counter = () => {
   const countertabs = [
     { number: 20, text: "EVENTS", suffix: "+" },
@@ -10,26 +12,30 @@ export const Counter = () => {
     { number: 500, text: "PRIZES", suffix: "+" }, //6000+
   ];
   return (
-    <div className="landing-counter-container">
-      {countertabs.map((tab) => (
-        // eslint-disable-next-line react/jsx-key
-        <div className="landing-counter-subcontainer">
-          <CountUp end={tab.number} suffix={tab.suffix} duration={1.5}>
-            {({ countUpRef, start }) => (
-              <VisibilitySensor onChange={start} delayedCall>
-                <span ref={countUpRef} />
-              </VisibilitySensor>
-            )}
-          </CountUp>
-          <img
-            className="landing-counter-svg"
-            src="vector.png"
-            alt="landing counter image"
-          />
-          <span className="landing-counter-text">{tab.text}</span>
-        </div>
-      ))}
-    </div>
+    <>
+      <Divider />
+      <div className="landing-counter-container">
+        {countertabs.map((tab) => (
+          // eslint-disable-next-line react/jsx-key
+          <div className="landing-counter-subcontainer">
+            <CountUp end={tab.number} suffix={tab.suffix} duration={1.5}>
+              {({ countUpRef, start }) => (
+                <VisibilitySensor onChange={start} delayedCall>
+                  <span ref={countUpRef} />
+                </VisibilitySensor>
+              )}
+            </CountUp>
+            <img
+              className="landing-counter-svg"
+              src="vector.png"
+              alt="landing counter image"
+            />
+            <span className="landing-counter-text">{tab.text}</span>
+          </div>
+        ))}
+      </div>
+      <Divider />
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,69 @@
+import React, { useState, useRef, useLayoutEffect } from "react";
 import Counter from "../components/section/counter";
+
 export default function Home() {
+  const [animate, doAnimate] = useState(false);
+
+  const ourRef = useRef(null);
+
+  const disableScrolling = () => {
+    const x = window.scrollX;
+    const y = window.scrollY;
+    window.onscroll = function () {
+      window.scrollTo(x, y);
+    };
+  };
+
+  const enableScrolling = () => {
+    window.onscroll = function () {};
+  };
+
+  const onScroll = () => {
+    if (!process.browser) return;
+    // const scrollPosition = window.scrollY;
+    // console.log("yoyo:", scrollPosition, topPosition)
+    doAnimate(true);
+    window.scrollTo(0, 0);
+  };
+
+  useLayoutEffect(() => {
+    if (!process.browser) return;
+    disableScrolling();
+    window.addEventListener("scroll", () => {
+      doAnimate(true);
+      setTimeout(() => enableScrolling(), 2000);
+    });
+  }, []);
+
   return (
-    <div className="h-screen overflow-hidden bg-black text-white flex flex-col space-y-8 items-center justify-center">
+    <div>
+      <section className="flex items-start justify-between landing overflow-hidden">
+        <img src="/leftLine.svg" alt="leftLine" />
+
+        <div className="flex flex-col items-center text-center mx-auto">
+          <p className="text-lg text-white font-bold uppercase">
+            21-23 January
+          </p>
+          <h1 className="text-4xl text-white font-bold uppercase">
+            E-summit '22
+          </h1>
+          <img src="/headLine.svg" id="headLine" alt="headLine" />
+          <div className="flex justify-center relative">
+            <div className="bg-dark text-white absolute w-full h-full z-10 mix-blend-multiply	"></div>
+            <div className="bg-shine text-white absolute w-full h-full z-10 mix-blend-multiply	"></div>
+            <img
+              ref={ourRef}
+              id="landingLogo"
+              className={animate && "landing-logo-animate"}
+              src="/landingLogo.png"
+              alt="logo"
+            />
+          </div>
+        </div>
+
+        <img src="/rightLine.svg" alt="rightLine" />
+      </section>
+      <section className="text-white">speakers</section>
       <Counter />
     </div>
   );
