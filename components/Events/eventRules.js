ab72021f9435d699d1f7aaa6fd68e0e4c14ab556
pranav <pranav_a@ece.iitr.ac.in>
2021-12-07 13:31:07 +0530

    minor changes

--- components/Events/eventRules.js
+++ components/Events/eventRules.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
-
+import React from "react";
+import Image from "next/image";
 function Eventrules(Event_Arr_rules) {
   return (
-    <div>
-      {console.log(Event_Arr_rules.Eventrules)}
-      <div className="bg-black text-white  Speaker_page_container">
+    <div className="Event-rules-main-container">
+      <div className="Event-rules-head">Rules</div>
+      <div className="Event-rules-list">
         {Event_Arr_rules.Eventrules.map((det) => {
-          return <div className="event_rule_loop">{det.rule}</div>;
+          return (
+            <div className="event_rule_loop">
+              <Image src="../../public/Ellipse91.png" /> {det.rule}
+            </div>
+          );
         })}
       </div>
     </div>
