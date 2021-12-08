43e1dbf6952033ebb93f1ba1cb06ca3277875e7c
Lomsha Agrawal <75161534+Lomsha123@users.noreply.github.com>
2021-12-08 23:37:14 +0530

    create resource_component for dashboard (#35)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Counter from "../components/section/counter";
-import TeamMemberCard from "../components/teamCard";
 export default function Home() {
   return (
     <div className="h-screen overflow-hidden bg-black text-white flex flex-col space-y-8 items-center justify-center">
