6b1fac9c0dfab3175441c06ae0a646ce5132abca
Akash Tiwari <akash_t@bt.iitr.ac.in>
2021-12-08 16:00:41 +0530

    create-dashboard-event-card

---
+++ components/dashboardEventCard.js
@@ -0,0 +1,27 @@
+import CustomGradientBtn from "./customGradientBtn";
+export const EventDashboardCard = () => {
+  let isregistered = true;
+  return (
+    <div className="dashboard-event-main-container">
+      <div className="dashboard-event-head">
+        <div className="dashboard-event-name">Ideastorm</div>
+        <div className={isregistered ? "dashboard-registered-event" : null}>
+          <p>{isregistered ? "Registered" : null}</p>
+        </div>
+      </div>
+      <div className="dashboard-event-desc">
+        E-Summit being the flagship event of ECell, is held annually brings
+        together the academic community, venture capitalists, new age
+        entrepreneurs and all those passionate about entrepreneurship to common
+        grounds and suggestions.
+      </div>
+      <div className="dashboard-event-visit-page">
+        <div className="dashboard-event-CustomGradientBtn">
+          <CustomGradientBtn color="black" text="Visit Page" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EventDashboardCard;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import EventDashboardCard from "../components/dashboardEventCard";
 import Counter from "../components/section/counter";
 import TeamMemberCard from "../components/teamCard";
 export default function Home() {
   return (
     <div className="h-screen overflow-hidden bg-black text-white flex flex-col space-y-8 items-center justify-center">
       <Counter />
+      <EventDashboardCard />
     </div>
   );
 }
