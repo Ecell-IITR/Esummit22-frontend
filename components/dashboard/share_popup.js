d65d5bb9bb5db3225973e9fdb2264755807a7176
Lomsha123 <lomsha@ce.iitr.ac.in>
2021-12-09 17:22:40 +0530

    create share-popup

---
+++ components/dashboard/share_popup.js
@@ -0,0 +1,125 @@
+import React from "react";
+
+export const Share = () => {
+  return (
+    <div className="mainDiv_behind">
+      <div className="mainDiv_front">
+        <div className="heading">
+          <div className="share_heading">
+            <h1>Share</h1>
+          </div>
+          <div className="close_btn">
+            <button>X</button>
+          </div>
+        </div>
+        <div className="subDiv_behind">
+          <div className="subDiv_front">
+            <p className="para">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. It is the standard dummy text.Lorem Ipsum is simply
+              dummy text of the printing and typesetting industry. It is the
+              standard dummy text.
+            </p>
+            <div className="copy_btn">
+              <h1>Copy</h1>
+            </div>
+          </div>
+        </div>
+        <div className="media_div">
+          <div className="single_media_div">
+            <a
+              href="https://www.facebook.com/ecelliitr"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="new-footer-section-contact-link"
+            >
+              <img
+                src="Facebook_img.png"
+                className="new-footer-section-contact-logo"
+                alt=""
+              />
+              <h1 className="media_title">Facebook</h1>
+            </a>
+          </div>
+          <div className="single_media_div">
+            <a
+              className="new-footer-section-contact-link"
+              href="https://twitter.com/ECell_IITR"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="LinkedIn_img.png"
+                className="new-footer-section-contact-logo"
+                alt=""
+              />
+              <h1 className="media_title">LinkedIn</h1>
+            </a>
+          </div>
+          <div className="single_media_div">
+            <a
+              href="https://www.instagram.com/ecelliitr/"
+              target="_blank"
+              className="new-footer-section-contact-link"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="Twitter_img.png"
+                // className="new-footer-section-contact-logo"
+                alt=""
+              />
+              <h1 className="media_title">Twitter</h1>
+            </a>
+          </div>
+          <div className="single_media_div">
+            <a
+              className="new-footer-section-contact-link"
+              href="https://www.linkedin.com/company/esummit"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="Instagram_img.png"
+                className="new-footer-section-contact-logo"
+                alt=""
+              />
+              <h1 className="media_title">Instagram</h1>
+            </a>
+          </div>
+          <div className="single_media_div">
+            <a
+              className="new-footer-section-contact-link"
+              href="https://www.youtube.com/channel/UCf1py66Ov1easJAswwjxQZw"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="Whatsapp_img.png"
+                className="new-footer-section-contact-logo"
+                alt=""
+              />
+              <h1 className="media_title">Whatsapp</h1>
+            </a>
+          </div>
+          <div className="single_media_div">
+            <a
+              className="new-footer-section-contact-link"
+              href="https://twitter.com/ECell_IITR"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="Email_img.png"
+                className="new-footer-section-contact-logo"
+                alt=""
+              />
+              <h1 className="media_title">Email</h1>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Share;
