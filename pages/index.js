d52edd3704f4f450dbdd668f79ff92d67f9261e5
Akash Tiwari <akash_t@bt.iitr.ac.in>
2021-12-11 02:02:51 +0530

    create-email-verification-card

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import VerifyEmailForTask from "../components/dashboard/emailVerify";
 import Counter from "../components/section/counter";
 export default function Home() {
   return (
     <div className="h-screen overflow-hidden bg-black text-white flex flex-col space-y-8 items-center justify-center">
       <Counter />
+      <VerifyEmailForTask />
     </div>
   );
 }
