9aac4cdc568954ea6e986b95d31280f4a41045b2
pranav <pranav_a@ece.iitr.ac.in>
2021-12-13 16:03:40 +0530

    Ca navbar added

---
+++ components/dashboard/Navbar.js
@@ -0,0 +1,73 @@
+import Link from "next/link";
+import React, { useState } from "react";
+export default function Navbar() {
+  const [activelist, setactivelist] = useState([true, false, false]);
+
+  return (
+    <div className="Ca_sidebar">
+      <div className="nav-items">
+        <ul>
+          <li>
+            <div
+              className={
+                activelist[0]
+                  ? "nav-text-container-active"
+                  : "nav-text-container-inactive"
+              }
+            >
+              <div
+                className="nav_item_text"
+                onClick={() => {
+                  setactivelist([true, false, false]);
+                }}
+              >
+                <div className={activelist[0] ? "text_black" : ""}>Tasks</div>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div
+              className={
+                activelist[1]
+                  ? "nav-text-container-active"
+                  : "nav-text-container-inactive"
+              }
+            >
+              <div
+                className="nav_item_text"
+                onClick={() => {
+                  setactivelist([false, true, false]);
+                }}
+              >
+                <div className={activelist[1] ? "text_black" : ""}>
+                  Leaderboard
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div
+              className={
+                activelist[2]
+                  ? "nav-text-container-active"
+                  : "nav-text-container-inactive"
+              }
+            >
+              <div
+                className="nav_item_text"
+                onClick={() => {
+                  setactivelist([false, false, true]);
+                }}
+              >
+                <div className={activelist[2] ? "text_black" : ""}>
+                  CA Rulebook
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
