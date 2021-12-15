c3bdfaf7fb319ca5b9677c238907056ddeea43cd
pranav <76247369+pranav-iitr@users.noreply.github.com>
2021-12-15 16:44:18 +0530

    api integration

--- components/task.js
+++ components/task.js
@@ -1,19 +1,17 @@
 import CustomGradientBtn from "./customGradientBtn";
 
-export const Task = () => {
+export const Task = ({description,Due_date,points}) => {
   return (
     <div className="tasks-container">
       <div className="tasks-container-discription">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. It is the standard dummy text.Lorem Ipsum is simply dummy text
-        of the printing and typesetting industry. It is the standard dummy text.
+        {description}
       </div>
-      <div className="tasks-container-date">Due Date: 25/11/20</div>
+      <div className="tasks-container-date">Due Date: {Due_date}</div>
       <div className="tasks-container-share">
         <div className="tasks-container-share-button">
           <CustomGradientBtn text="Share" color="black" />
         </div>
-        <div className="tasks-container-share-score">10 points</div>
+        <div className="tasks-container-share-score">{points} points</div>
         <div className="tasks-container-share-button">
           <CustomGradientBtn text="Upload" color="black" />
         </div>
