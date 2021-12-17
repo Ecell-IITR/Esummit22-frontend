c0ebc0d5f137da96234a6cd0a9507d6452267961
pranav <pranav_a@ece.iitr.ac.in>
2021-12-17 17:24:32 +0530

    task update

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({ allEvents, allSpeakers }) {
           <CustomGradientBtn text="View All" color="black" />
         </div>
       </Link>
-      <Counter />
+      <Counter />{"  "}
       <HomeEvents allEvents={allEvents} />
       <Link href="/events" passHref>
         <div className="home-view-all-button">
