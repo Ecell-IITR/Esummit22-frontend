c8407d5e13222922a8466167b124b4d0718feb78
Darkshredder <srivastavayash58@gmail.com>
2021-12-17 22:52:09 +0530

    Linted
    Signed-off-by: Darkshredder <srivastavayash58@gmail.com>

--- components/events-details-page/EventsFaq.js
+++ components/events-details-page/EventsFaq.js
@@ -1,8 +1,6 @@
 import Accordion from "../faq/accordian";
 
 function EventsFaq({ detailsEvents }) {
-  
-
   return (
     <div className="events_details_faq">
       {detailsEvents[0].event_faqs.map((faq, id) => (
