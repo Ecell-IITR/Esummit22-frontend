76364efc00f7461b7c8069239a3069013a8b8ed2
pranav <pranav_a@ece.iitr.ac.in>
2021-12-23 16:10:58 +0530

    non ca navbar edited

--- components/dashboard/dashboardEventCard.js
+++ components/dashboard/dashboardEventCard.js
@@ -1,19 +1,16 @@
 import CustomGradientBtn from "../customGradientBtn";
-export const EventDashboardCard = () => {
-  let isregistered = true;
+export const EventDashboardCard = ({title,desc,isregistered}) => {
+  
   return (
     <div className="dashboard-event-main-container">
       <div className="dashboard-event-head">
-        <div className="dashboard-event-name">Ideastorm</div>
+        <div className="dashboard-event-name">{title}</div>
         <div className={isregistered ? "dashboard-registered-event" : null}>
           <p>{isregistered ? "Registered" : null}</p>
         </div>
       </div>
       <div className="dashboard-event-desc">
-        E-Summit being the flagship event of ECell, is held annually brings
-        together the academic community, venture capitalists, new age
-        entrepreneurs and all those passionate about entrepreneurship to common
-        grounds and suggestions.
+        {desc}
       </div>
       <div className="dashboard-event-visit-page">
         <div className="dashboard-event-CustomGradientBtn">
---
+++ pages/nonca/events.js
@@ -0,0 +1,11 @@
+import NoncaNavbar from "../../components/nonca/nonca_navbar";
+
+const CapLeaderboard = () => {
+  return (
+    <div className="nonca-main-container">
+      <NoncaNavbar />
+    </div>
+  );
+};
+
+export default CapLeaderboard;
