4281107d8170fef57f950ad07472c151d9beb880
Utkarsh Raj <you@example.comutkarsh2304003@gmail.com>
2021-12-25 04:56:03 +0530

    apply now popup

--- components/Events/EventHead/eventHeadCard.js
+++ components/Events/EventHead/eventHeadCard.js
@@ -1,5 +1,71 @@
 import TransparentGradientButton from "../../transparentGradientButton";
-export const EventHead = ({ detailsEvents }) => {
+import Link from "next/link";
+
+export const EventHead = ({
+  detailsEvents,
+  CompetitiveEvents,
+  SpeakingEvents,
+  WorkshopEvents,
+  Show,
+  setShow,
+}) => {
+  const getPreviousEventLink = () => {
+    let previousLink = "";
+    CompetitiveEvents.map((event, index) => {
+      if (event.id === detailsEvents[0].id) {
+        if (index === 0) {
+          previousLink =
+            CompetitiveEvents[CompetitiveEvents.length - 1].end_point;
+        } else {
+          previousLink = CompetitiveEvents[index - 1].end_point;
+        }
+      }
+    });
+    return previousLink;
+  };
+  const getPreviousEventName = () => {
+    let previousName = "";
+    CompetitiveEvents.map((event, index) => {
+      if (event.id === detailsEvents[0].id) {
+        if (index === 0) {
+          previousName =
+            CompetitiveEvents[CompetitiveEvents.length - 1].event_name;
+        } else {
+          previousName = CompetitiveEvents[index - 1].event_name;
+        }
+      }
+    });
+    return previousName;
+  };
+
+  const getNextEventLink = () => {
+    let NextLink = "";
+    CompetitiveEvents.map((event, index) => {
+      if (event.id === detailsEvents[0].id) {
+        if (index === CompetitiveEvents.length - 1) {
+          NextLink = CompetitiveEvents[0].end_point;
+        } else {
+          NextLink = CompetitiveEvents[index + 1].end_point;
+        }
+      }
+    });
+    return NextLink;
+  };
+
+  const getNextEventName = () => {
+    let NextName = "";
+    CompetitiveEvents.map((event, index) => {
+      if (event.id === detailsEvents[0].id) {
+        if (index === CompetitiveEvents.length - 1) {
+          NextName = CompetitiveEvents[0].event_name;
+        } else {
+          NextName = CompetitiveEvents[index + 1].event_name;
+        }
+      }
+    });
+    return NextName;
+  };
+
   return (
     <div className="event-head">
       <div className="event-image-container">
@@ -10,14 +76,22 @@ export const EventHead = ({ detailsEvents }) => {
         />
         <div>
           <button className="previous-event-button">
-            <img className="previous-next" src="/previous.png" alt="previous" />
-            previous
+            <Link href={`/events/${getPreviousEventLink()}`} passHref>
+              <img
+                className="previous-next"
+                src="/previous.png"
+                alt="previous"
+              />
+            </Link>
+            {getPreviousEventName()}
           </button>
         </div>
         <div>
           <button className="next-event-button">
-            <img className="previous-next" src="/next.png" alt="next" />
-            next
+            <Link href={`/events/${getNextEventLink()}`} passHref>
+              <img className="previous-next" src="/next.png" alt="next" />
+            </Link>
+            {getNextEventName()}
           </button>
         </div>
         <div className="centered">
@@ -28,7 +102,13 @@ export const EventHead = ({ detailsEvents }) => {
               __html: detailsEvents[0].card_description,
             }}
           />
-          <TransparentGradientButton text="apply now" />
+          <div
+            onClick={() => {
+              setShow(true);
+            }}
+          >
+            <TransparentGradientButton text="apply now" />
+          </div>
         </div>
       </div>
     </div>
