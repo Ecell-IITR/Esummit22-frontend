2a68b5f3ee8f5bc9d38589da3f959ad897a7a254
Lomsha123 <lomsha@ce.iitr.ac.in>
2021-12-25 23:48:02 +0530

    removed APIFetch_nonCA_resources

--- pages/nonca/index.js
+++ pages/nonca/index.js
@@ -1,27 +1,13 @@
 import NoncaNavbar from "../../components/nonca/nonca_navbar";
-import React, { useEffect, useState } from "react";
-import FetchApi from "../../utils/fetchAPI";
-import { NON_CA_PROFILE_API } from "../../utils/APIs";
+import React from "react";
 import NonCAResources from "../../components/nonca/nonca_resources";
-import { getAuthToken } from "../../utils";
 
 const Home = () => {
-
-  const [nonCA_resourcesList, setNonCA_ResourcesList] = useState([]);
-
-  useEffect(() => {
-    FetchApi("get", NON_CA_PROFILE_API, null, getAuthToken())
-      .then((res) => {
-        setNonCA_ResourcesList(res.data[0].resources_uploaded);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
     <div className="nonca-main-container">
       <NoncaNavbar />
       <div>
-        <NonCAResources nonCA_resources_list={nonCA_resourcesList[0]}/>
+        <NonCAResources />
       </div>
     </div>
   );
