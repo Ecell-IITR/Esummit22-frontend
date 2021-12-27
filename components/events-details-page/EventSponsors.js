de718e9d6e4bc999602221b640ef89733b8d02b6
pranav <pranav_a@ece.iitr.ac.in>
2021-12-27 18:33:34 +0530

    date display update

--- components/events-details-page/EventSponsors.js
+++ components/events-details-page/EventSponsors.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-export const EventSponsor = ({ img , title }) => {
+export const EventSponsor = ({ detailsEvents }) => {
   return (
     <div className="sponsor_main">
       <div className="img_div_behind">
         <div className="img_div_front">
-          <img src={img} alt=" "></img>
+          <img src={detailsEvents[0].event_partners[0].image} alt=" "></img>
         </div>
       </div>
       <div className="title_sponsor">
-        <h1>{title}</h1>
+        <h1>{detailsEvents[0].event_partners[0].title}</h1>
       </div>
     </div>
   );
