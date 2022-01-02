ebb8d4f055e0481712516e9960c89757f17fb5d1
pranav <pranav_a@ece.iitr.ac.in>
2022-01-02 15:44:18 +0530

    events seo

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,7 @@
 import EventCard from "../../components/Events/eventCard";
 import { ALL_EVENTS_API } from "../../utils/APIs";
 import Footer from "../../components/footer";
+import { NextSeo } from "next-seo";
 
 export const Events = ({
   CompetitiveEvents,
@@ -9,6 +10,36 @@ export const Events = ({
 }) => {
   return (
     <div>
+       <NextSeo
+        title={"E-Summit 22 | Events " }
+        description={"Events including but not limted to Productathon, Ideastorm , Sale-Able, Investors' Vista, Startup Expo ,Design Case Challenge"}
+        canonical={`https://www.esummit.in/events`}
+        openGraph={{
+          url: `https://www.esummit.in/events`,
+          title: "E-Summit 22 | Events ",
+          description: "Events including but not limted to Productathon, Ideastorm , Sale-Able, Investors' Vista, Startup Expo ,Design Case Challenge",
+          images: [
+            {
+              url: "https://www.esummit.in/Esummit_logo.png",
+              alt: "logo",
+              type: "image/jpeg/png",
+            },
+          ],
+          site_name: `Events | E-Summit 22 IITR`,
+        }}
+        additionalMetaTags={ [
+          {
+            name: "keywords",
+            content:
+              "esummit, e-summit, iit roorkee, conclave, Startup expo, Ideastorm, Investors Vista ,Saleable, product-a-thon ,speaker ,events ",
+          },
+        ]
+        
+        }
+    
+
+
+      />
       <div className="events-page-heading">
         <h1>EVENTS</h1>
       </div>
