37626f3d2f41c1c8e743f07b8edd491fe7d86e4d
pranav <pranav_a@ece.iitr.ac.in>
2022-01-17 23:29:48 +0530

    name spacing update

--- components/nonca/noncaprofile.js
+++ components/nonca/noncaprofile.js
@@ -3,8 +3,28 @@ import { useState, useEffect } from 'react';
 import FetchApi from '../../utils/fetchAPI';
 import { NON_CA_PROFILE_API } from '../../utils/APIs';
 import { getAuthToken } from '../../utils';
-
-export const NProfile = ({ name = '', points, id, college, rank }) => {
+function name_editor(text){
+  let string_arr=text.split(" ")
+  console.log()
+  if(string_arr.length==1){
+    return text.slice(0,16) + ".."
+  }else {
+  var x=16;
+  var return_text=""
+  for(var i=0;i<string_arr.length;i++)
+     if (x-string_arr[i].length>0){
+       return_text+=string_arr[i]+" "
+       x-=string_arr[i].length
+     }
+     else{
+      return_text+="</br>"+string_arr[i]
+      x=16-string_arr[i].length
+     }
+     return return_text
+  }
+  
+}
+export const NProfile = () => {
   const [caData, setcaData] = useState([]);
   useEffect(() => {
     FetchApi('get', NON_CA_PROFILE_API, null, getAuthToken())
@@ -22,8 +42,10 @@ export const NProfile = ({ name = '', points, id, college, rank }) => {
         <div className='nprofile-image'>
           <p>{caData?.profile?.full_name[0]}</p>
         </div>
-        <div className='nprofile-details-name-one'>
-          {caData?.profile?.full_name}
+        <div className='nprofile-details-name-one' dangerouslySetInnerHTML={{
+                      __html: caData?.profile?.full_name.length>16? name_editor(caData?.profile?.full_name):caData?.profile?.full_name,
+                    }}>
+          
         </div>
 
         <div className='nprofile-white-text'> {caData?.college}</div>
